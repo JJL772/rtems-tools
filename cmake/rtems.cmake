5aa193ff5d40b9b20e87c3f9178030f6ad50b516
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2025-03-02 11:45:49 -0800

    modify install rules again

--- cmake/rtems.cmake
+++ cmake/rtems.cmake
@@ -1,5 +1,7 @@
 # Additional tools for RTEMS
 
+include(GNUInstallDirs)
+
 function(rtems_add_executable TARGET )
     add_executable(
         ${TARGET} ${ARGN}
@@ -16,4 +18,25 @@ function(rtems_add_executable TARGET )
         "${TARGET}-boot" ALL
         DEPENDS "${CMAKE_BINARY_DIR}/${TARGET}.boot"
     )
+    
+    # Install to an EPICS-style "shared" prefix
+    if (SHARED_PREFIX)
+        install(
+            TARGETS "${TARGET}"
+            
+            RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}/RTEMS-${RTEMS_BSP}"
+            LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}/RTEMS-${RTEMS_BSP}"
+            ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}/RTEMS-${RTEMS_BSP}"
+        )
+        install(
+            FILES "${CMAKE_BINARY_DIR}/${TARGET}.boot"
+            DESTINATION "${CMAKE_INSTALL_BINDIR}/RTEMS-${RTEMS_BSP}"
+        )
+    else()
+        install(TARGETS "${TARGET}-boot" "${TARGET}")
+        install(
+            FILES "${CMAKE_BINARY_DIR}/${TARGET}.boot"
+            DESTINATION "${CMAKE_INSTALL_BINDIR}"
+        )
+    endif()
 endfunction()
