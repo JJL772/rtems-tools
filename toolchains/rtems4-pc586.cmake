a594e4150cbd713ebc3f9fbbc7cd45a8f757a7cd
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2025-07-30 00:18:55 -0700

    toolchains: Update RTEMS 4.10.2 toolchains to rtems_p5

--- toolchains/rtems4-pc586.cmake
+++ toolchains/rtems4-pc586.cmake
@@ -3,7 +3,7 @@
 set(RTEMS_ARCH "i386")
 set(RTEMS_VERSION 4.10.2)
 set(RTEMS_BSP "pc586")
-set(RTEMS_SUBDIR "rtems_p4")
+set(RTEMS_SUBDIR "rtems_p5")
 set(RTEMS_NETWORK_STACK "LEGACY")
 set(RTEMS_BSP_CFLAGS "-march=pentium -O2 -g -Wall -Wimplicit-function-declaration -Wstrict-prototypes -Wnested-externs")
 set(RTEMS_BSP_LDFLAGS "-Wl,-Ttext,0x100000")
--- toolchains/rtems4-uC5282.cmake
+++ toolchains/rtems4-uC5282.cmake
@@ -3,7 +3,7 @@
 set(RTEMS_ARCH "m68k")
 set(RTEMS_VERSION 4.10.2)
 set(RTEMS_BSP "uC5282")
-set(RTEMS_SUBDIR "rtems_p4")
+set(RTEMS_SUBDIR "rtems_p5")
 set(RTEMS_NETWORK_STACK "LEGACY")
 set(RTEMS_BSP_CFLAGS "-mcpu=5282 -O2 -g -Wimplicit-function-declaration -Wstrict-prototypes -Wnested-externs")
 include ("${CMAKE_CURRENT_LIST_DIR}/rtems-toolchain.cmake")
